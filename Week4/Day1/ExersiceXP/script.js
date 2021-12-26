ce7901431db9844e39f6d7c095e3a95653c5d827
michaelsegan <michael.segan1@gmail.com>
2021-12-26 22:23:48 +0200

    d

---
+++ Week4/Day1/ExersiceXP/script.js
@@ -0,0 +1,83 @@
+// Exersice 1
+
+
+// // //1. Analyse the code below, and predict what will be the value of a in all the following functions.
+// // //2. Write your prediction as comments in a js file.
+// // // #1
+// // function q1() {
+// //     var a = 5;
+// //     if(a > 1) {
+// //         a = 3;
+// //     }
+// //     alert(a);
+// // }
+
+
+// // // value of a is 3 because the value chnges in the local scope
+
+// // //#2
+// // // var a = 0;
+// // // function q2() {
+// // //     a = 5;
+
+// // // }
+// // // the value will be 5 because is declered outside of the function and then value wa changed
+
+// // // function q22() {
+// // //     alert(a);
+// // // }
+
+// // // the value of a in this function will be 0 because it daclared in the global scope and didnt changed in the local scope
+
+
+// // //#3
+// // // function q3() {
+// // //     window.a = "hello";
+// // //     console.log(window.a);
+// // // }
+// // // q3 ();
+
+
+// // // function q32() {
+// // //     alert(a);
+// // // }
+// // // q32 ();
+
+// // //#4
+// // // var a = 1;
+// // // function q4() {
+// // //     var a = "test";
+// // //     alert(a);
+// // // }
+// // // q4();
+// // // // the a will be "text" because it was declared in the global scope
+
+// // // //#5
+// // var  = 6;
+// // if (true) {
+// //     var a = 2;
+// //     alert(a);
+// // }
+// // alert(a);
+// // // a will be 5 because a inside a statment is like a new variable
+
+// Exercise 2 : Ternary Operator
+
+
+// function experiencePoints(winIt) {
+//    let  experiencePoints = winIt >10 ? "win" : "loose";
+//    document.body.textContent = experiencePoints
+
+         
+// }
+
+// experiencePoints(20);
+
+let colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+
+
+
+
+
+
+
