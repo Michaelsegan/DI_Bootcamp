5ec6942a4a51edeffefdcc9904a3c9cbdc71bda5
michaelsegan <michael.segan1@gmail.com>
2022-01-01 20:11:01 +0200

    m

---
+++ Week4/Day5/DailyChallenge/script.js
@@ -0,0 +1,12 @@
+let isAnagram = function (str1,str2){
+    let sorted1 = str1.split('').sort().join('').toLowerCase();
+    let sorted2 = str2.split('').sort().join('').toLowerCase();
+    return (sorted1 === sorted2)
+
+}
+
+
+console.log(isAnagram('hello', 'jello'));
+console.log(isAnagram('hello','loelh'));
+console.log(isAnagram('shalom','shaolm'));
+console.log(isAnagram('lama','kaha'));
