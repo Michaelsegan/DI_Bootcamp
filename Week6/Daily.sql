04600abf3ae78430b7758d7c2ccfd493a9b69e14
michaelsegan <michael.segan1@gmail.com>
2022-01-09 18:48:27 +0200

    new matirial

---
+++ Week6/Daily.sql
@@ -0,0 +1,5 @@
+
+ SELECT * FROM actors
+-- SELECT COUNT (first_name) FROM actors;
+-- INSERT INTO actors (first_name,last_name,date_birth,number_oscars)
+-- VALUES ('Rayen','Raynolds','1984-08-09',2);
