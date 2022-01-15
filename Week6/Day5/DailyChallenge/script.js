41ae0ec8d22eeeeafdc7a91b08ca1734b670f1d1
michaelsegan <michael.segan1@gmail.com>
2022-01-15 20:59:42 +0200

    k

---
+++ Week6/Day5/DailyChallenge/script.js
@@ -0,0 +1,13 @@
+let form = document.querySelectorAll('#form input');
+let btn = document.getElementById('btn-save');
+// arrying the node list 
+let arrForm = Array.from(form);
+//looping through the array
+
+//add event listener to btn and function
+console.log(arrForm);
+
+
+
+
+
