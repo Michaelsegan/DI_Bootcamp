d6e3f98316a7c2f49e9363703f0a1415484fcdd1
michaelsegan <michael.segan1@gmail.com>
2022-01-18 18:04:04 +0200

    new

---
+++ Week7/Day3/Exercises/server.js
@@ -0,0 +1,44 @@
+
+//1. 
+
+
+// const express = require('express');
+
+// const app = express();
+
+// app.use((re,res,next)=>{
+//     console.log('some text to see');
+//     next();
+// })
+
+// app.get('/',(req, res) => {
+
+//     res.send('');
+// });
+// app.listen(3000);
+
+
+//2. 
+
+const fs = require('fs');
+
+// fs.writeFile('hello','hello Aichay',function(err){
+//     console.log('somthing');
+// })
+
+// //3. 
+
+// fs.appendFile('hello', 'adding new data', function (err) {
+//   if (err) {
+//     // append failed
+//   } else {
+//     // done
+//   }
+// })
+
+
+//3. 
+const path = './hello';
+fs.unlinkSync(path)
+
+
